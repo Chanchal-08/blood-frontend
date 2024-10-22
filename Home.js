a08193d8d74c93c39d0f62b212b5e0cdbc3fc2a8
Chanchal-08 <114850365+Chanchal-08@users.noreply.github.com>
2024-10-23 00:24:46 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Blood Donation App</h1>
+      <p>Find and register donors easily using this platform. Your contribution can save lives!</p>
+    </div>
+  );
+};
+
+export default Home;
